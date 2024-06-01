5703e6166c20ab3f1f91174bc5cc4ffcf16df25f
Anastasiia Tarasiuk <111896564+anastasiiatarasiuk@users.noreply.github.com>
2024-06-01 11:19:24 +0300

    add pagination

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
 import { UnsplashAPI } from './js/UnsplashAPI';
+import { createGalleryCard } from './js/render-function';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.min.css';
+
+const jsGallery = document.querySelector('.js-gallery');
+const container = document.getElementById('tui-pagination-container');
+const pagination = new Pagination(container, {
+  totalItems: 0,
+  itemsPerPage: 12,
+  visiblePages: 5,
+  page: 1,
+});
+
+const page = pagination.getCurrentPage();
+
 const api = new UnsplashAPI();
-api.getPopularImages(1).then(data => console.log(data));
+api.getPopularImages(page).then(data => {
+  const markup = createGalleryCard(data.results);
+  pagination.reset(data.total);
+  jsGallery.innerHTML = markup;
+});
