b57631eb74f5fc00019d75ee61bab961b55cf778
Qerilod <140080894+Qerilod@users.noreply.github.com>
2024-06-01 11:53:34 +0300

    comm

--- src/js/UnsplashAPI.js
+++ src/js/UnsplashAPI.js
@@ -4,6 +4,7 @@ import iziToast from 'izitoast';
 export class UnsplashAPI {
   #BASE_URL = 'https://api.unsplash.com/search/photos';
   #API_KEY = 'gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58';
+  #query = '';
   async getPopularImages(page) {
     const url = `${
       this.#BASE_URL
@@ -18,5 +19,21 @@ export class UnsplashAPI {
         message: 'something went wrong',
       });
     }
+  }
+  async getImagesByQuery(page) {
+    const url = `${this.#BASE_URL}?query=${
+      this.#query
+    }&page=${page}&per_page=12&orientation=portrait&client_id=${this.#API_KEY}`;
+    try {
+      const { data } = await axios(url);
+      return data;
+    } catch (error) {
+      iziToast.error({
+        message: 'something went wrong',
+      });
+    }
+  }
+  set query(newQuery) {
+    this.#query = newQuery;
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import iziToast from 'izitoast';
 import { UnsplashAPI } from './js/UnsplashAPI';
 import { createGalleryCard } from './js/render-function';
 import Pagination from 'tui-pagination';
@@ -5,6 +6,7 @@ import 'tui-pagination/dist/tui-pagination.min.css';
 
 const jsGallery = document.querySelector('.js-gallery');
 const container = document.getElementById('tui-pagination-container');
+const formEl = document.querySelector('.js-search-form');
 const pagination = new Pagination(container, {
   totalItems: 0,
   itemsPerPage: 12,
@@ -19,4 +21,28 @@ api.getPopularImages(page).then(data => {
   const markup = createGalleryCard(data.results);
   pagination.reset(data.total);
   jsGallery.innerHTML = markup;
+});
+pagination.on('afterMove', event => {
+  const currentPage = event.page;
+  console.log(currentPage);
+  api.getPopularImages(currentPage).then(data => {
+    const markup = createGalleryCard(data.results);
+    jsGallery.innerHTML = markup;
+  });
+});
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+  const searchQuery = event.target.elements.query.value.trim();
+  if (searchQuery === '') {
+    iziToast.info({
+      message: 'enter text',
+    });
+    return;
+  }
+  api.query = searchQuery;
+  api.getImagesByQuery(page).then(data => {
+    const markup = createGalleryCard(data.results);
+    jsGallery.innerHTML = markup;
+    pagination.reset(data.total);
+  });
 });
